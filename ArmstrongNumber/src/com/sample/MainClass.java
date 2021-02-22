00a7183e89f63a6f5f6b8ca41a8c9068d7a9fa6c
erwerw52 <erwerw52@gmail.com>
2021-02-22 23:48:27 +0800

    add Arms

---
+++ ArmstrongNumber/src/com/sample/MainClass.java
@@ -0,0 +1,27 @@
+package com.sample;
+
+import java.util.*;
+
+public class MainClass {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+		list.add(a);
+		list.add(b);
+		// 使用 List interface 中所提供的方法列出所有元素
+		for (int i=a;a<=b;a++){
+                    
+                    System.out.println(""+a);
+                }
+
+		
+		
+	}
+        
+    
+}    
+
+  
